73ff8a06f35330dedee7f2e798236173eaca6d1e
DOOM\antonio.silva <antonio.silva@papersoft.pt>
2017-05-26 22:38:26 +0100

    Class diagram for Default Statistics

--- TestNimatorCouchBase/TestCheckHttpCaller.cs
+++ TestNimatorCouchBase/TestCheckHttpCaller.cs
@@ -9,15 +9,51 @@ namespace TestNimatorCouchBase
     [TestClass]
     public class TestCheckHttpCaller
     {
+        private CheckHttpCallerParameters DefaultHttpCallerParameters;
+
+        [TestInitialize]
+        public void Init()
+        {
+            DefaultHttpCallerParameters = new CheckHttpCallerParameters("http://localhost:8091/pools/default", new HttpBasicAuthenticator("supertoino", "OcohoW*99"));
+        }
+
         [TestMethod]
         public void TestCheckHttpCallerGetOk()
-        {
-            var httpCallerParameters = new CheckHttpCallerParameters("http://localhost:8091/pools/default", new HttpBasicAuthenticator("supertoino","OcohoW*99"));
+        {            
+            var httpCallerParameters = DefaultHttpCallerParameters;
 
             CheckHttpCaller<CouchBaseDefaultStats> checkHttpCaller = new CheckHttpCaller<CouchBaseDefaultStats>(httpCallerParameters);
+
             var stats = checkHttpCaller.Call();
+
             Assert.AreNotEqual(stats, null);          
             Assert.AreEqual("supertoino", stats.ClusterName);                 
+        }
+
+        [TestMethod]
+        public void TestCheckHttpCallerGetNokInvalidUrl()
+        {
+            var httpCallerParameters = new CheckHttpCallerParameters("http://localhost:8091/pools/defaultt_", new HttpBasicAuthenticator("supertoino", "OcohoW*99"));
+
+            CheckHttpCaller<CouchBaseDefaultStats> checkHttpCaller = new CheckHttpCaller<CouchBaseDefaultStats>(httpCallerParameters);
+
+            var stats = checkHttpCaller.Call();
+
+            Assert.AreNotEqual(stats, null);
+            Assert.AreEqual(null, stats.ClusterName);
+        }
+
+        [TestMethod]
+        public void TestCheckHttpCallerGetNokInvalidCredentials()
+        {
+            var httpCallerParameters = new CheckHttpCallerParameters("http://localhost:8091/pools/defaultt_", new HttpBasicAuthenticator("supertoinoo", "OcohoW*99"));
+
+            CheckHttpCaller<CouchBaseDefaultStats> checkHttpCaller = new CheckHttpCaller<CouchBaseDefaultStats>(httpCallerParameters);
+
+            var stats = checkHttpCaller.Call();
+
+            Assert.AreNotEqual(stats, null);
+            Assert.AreEqual(null, stats.ClusterName);
         }
     }
 }
